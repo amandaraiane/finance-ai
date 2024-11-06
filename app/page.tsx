836ac13f8cfae5cdf191c9a6fb0f1d726a72ac1a
amanda-raiane <amanda@codifica.dev>
2024-11-06 20:47:25 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="m-4 flex bg-slate-800 text-slate-700">Olá Mundaa</h1>
+      <Button>Clique aqui</Button>
     </div>
   );
 }
