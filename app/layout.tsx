77246390a9b70115890239bdb797f3fbbdb874a6
amanda-raiane <amanda@codifica.dev>
2024-11-07 12:26:23 -0300

    chore: add user id property to transaction table

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Mulish } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
+import { Mulish } from "next/font/google";
 
 const mulish = Mulish({
   subsets: ["latin-ext"],
