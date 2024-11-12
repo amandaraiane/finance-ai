04c00726448fbe982b5c6504e30b2b48a78f9851
amanda-raiane <amanda@codifica.dev>
2024-11-12 13:49:19 -0300

    feat: add stripe webhook that hadles subscription

--- app/subscription/_components/acquire-plan-button.tsx
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -1,14 +1,19 @@
 "use client";
+
 import { Button } from "@/app/_components/ui/button";
 import { createStripeCheckout } from "../_actions/create-stripe-checkout";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 export function AcquirePlanButton() {
+  const { user } = useUser();
+
   async function handleAcquirePlanClick() {
     const { sessionId } = await createStripeCheckout();
 
     if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
-      throw new Error("Stripe public key not found");
+      throw new Error("Stripe publishable key not found");
     }
 
     const stripe = await loadStripe(
@@ -20,9 +25,22 @@ export function AcquirePlanButton() {
     }
     await stripe.redirectToCheckout({ sessionId });
   }
+
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan == "premium";
+  if (hasPremiumPlan) {
+    return (
+      <Button className="w-full rounded-full font-bold" variant="link">
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.emailAddresses[0].emailAddress}`}
+        >
+          Gerenciar plano
+        </Link>
+      </Button>
+    );
+  }
   return (
     <Button
-      className="w-full rounded-lg font-bold"
+      className="w-full rounded-full font-bold"
       onClick={handleAcquirePlanClick}
     >
       Adquirir plano
